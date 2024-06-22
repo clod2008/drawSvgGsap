5648045ac3727a6edd8e9477817f5778b4a2b077
Claudio Uccello <claudio@apsis.com.ar>
2024-06-22 15:36:27 -0300

    test

--- scripts.js
+++ scripts.js
@@ -7,10 +7,38 @@ document.addEventListener('DOMContentLoaded', () => {
     path.style.strokeDasharray = length;
     path.style.strokeDashoffset = -length;
 
-    // Crear una línea de tiempo
-    const tl = gsap.timeline()
+    // Preload de la imagen de fondo
+    function preloadImage(url) {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.onload = resolve;
+            img.onerror = reject;
+            img.src = url;
+        });
+    }
 
-    tl.to(path, { duration: 4, strokeDashoffset: 0 })
-    tl.to(imgBg, { duration: 3, opacity: 1 }, "-=2")
+    // URL de la imagen de fondo
+    const backgroundImageUrl = './imgBg.png'; // Asegúrate de que la ruta sea correcta
+
+    // Iniciar preloader
+    preloadImage(backgroundImageUrl)
+        .then(() => {
+            // Una vez que la imagen está cargada, procedemos con las animaciones
+
+            // Crear una línea de tiempo con GSAP
+            const tl = gsap.timeline();
+
+            tl.to(imgBg, { opacity: 0 })
+            // Añadir animación de strokeDashoffset al path
+            tl.to(path, { duration: 4, strokeDashoffset: 0 });
+
+            // Añadir fade-in del fondo a la línea de tiempo
+            tl.to(imgBg, { opacity: 0.001, duration: 1, delay: -3 })
+            tl.to(imgBg, { opacity: 1, duration: 2, delay: -1 })
+
+        })
+        .catch((error) => {
+            console.error('Error al cargar la imagen de fondo:', error);
+            // Manejar errores de carga de la imagen aquí, como mostrar un mensaje alternativo o cargar una imagen por defecto
+        });
 });
-
